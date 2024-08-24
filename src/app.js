d27efcf0347cdb81c6732e5b91d8586f4777cbea
DinaraMiller <diluv87@gmail.com>
2024-08-23 20:22:21 -0600

    Added search functionality via API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function weatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let currentCityElement = document.querySelector("#city");
+  currentCityElement.innerHTML = response.data.city;
+}
+
+function searchCityTemp(city) {
+  let apiKey = "b15d302eb82b9077a2o5t35a3bcfcf4f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(weatherData);
+}
+
 function weatherInCity(event) {
   event.preventDefault();
   let searchCityElement = document.querySelector("#search-city-input");
-  let currentCityElement = document.querySelector("#city");
-  currentCityElement.innerHTML = searchCityElement.value;
+
+  searchCityTemp(searchCityElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", weatherInCity);
+
+searchCityTemp("Denver");
